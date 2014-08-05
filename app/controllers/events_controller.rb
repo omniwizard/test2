1a2786163bef1b7068328ea7882063c9e20ee753
Александр Макаренко <omfxpr@gmail.com>
2014-08-05 13:20:55 +0400

    Все поменял

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,31 @@
+require 'date'
+require 'time'
+
+
 class EventsController < ApplicationController
   def new
   end
+
   def create
     @events = Events.new(events_params)
 
     @events.save
     redirect_to '/'
   end
+
   def edit
     @events = Events.find(params[:id])
   end
+
   def show
     @events = Events.find(params[:id])
   end
+
   def index
     @events = Events.all
 
   end
+
   def update
     @event = Events.find(params[:id])
 
@@ -26,27 +35,75 @@ class EventsController < ApplicationController
       render 'edit'
     end
   end
-  def list
 
-    # @events = Events.all
-    # events = []
-    # @events.each do |event|
-    #   events << {:id => event.id, :title => event.event_name, :start => "#{event.event_data.iso8601}", :end => "#{event.event_data.iso8601}", :allDay => false}
-    # end
+  def destroy
+    @event = Events.find(params[:id])
+
+    @event.delete
+    redirect_to '/'
+  end
+
+  def list
+    cal_start  = DateTime.parse(params['start'])
+    cal_end  = DateTime.parse(params['end'])
+
     if params['all_events'] == 'true'
       @events = Events.all
     else
-      @events = Events.where(:user => current_user)
+      @events = Events.where(:user => current_user.to_param)
     end
     events = []
-    @events.each do |event|
+    @events.each { |event|
+      i = cal_end - (event.event_data.iso8601).to_date
+      current = (event.event_data.iso8601).to_datetime
+      if event.period ==1
+        if i>0
+          z=1
+          while  z <= i.to_i do
+            events << {:id => event.id, :title => event.event_name, :start => "#{current + z}", :end => "#{current + z}", :allDay => false}
+            z+=1
+          end
+        end
+      elsif  event.period == 2
+        if i.to_i> 7
+          we = i.to_i/7
+          x = 1
+          while x <= we
+
+            events << {:id => event.id, :title => event.event_name, :start => "#{current + x.week}", :end => "#{current + x.week}", :allDay => false}
+            x+=1
+          end
+        end
+      elsif  event.period == 3
+        if i.to_i> 30
+          mon = i.to_i/30
+          x = 1
+          while x <= mon
+            events << {:id => event.id, :title => event.event_name, :start => "#{current + x.month}", :end => "#{current + x.month}", :allDay => false}
+            x+=1
+          end
+        end
+      elsif  event.period == 4
+          if i.to_i> 365
+            ya = i.to_i/365
+            x = 1
+            while x <= ya
+              current = (event.event_data.iso8601).to_date
+              events << {:id => event.id, :title => event.event_name, :start => "#{current + x.year}", :end => "#{current + x.year}", :allDay => false}
+              x+=1
+            end
+        end
+      end
       events << {:id => event.id, :title => event.event_name, :start => "#{event.event_data.iso8601}", :end => "#{event.event_data.iso8601}", :allDay => false}
-    end
+    }
+
+
     render :text => events.to_json
   end
+
   private
   def events_params
-    params.require(:events).permit(:event_name, :event_data , :user, :period )
+    params.require(:events).permit(:event_name, :event_data, :user, :period)
   end
 
 end
